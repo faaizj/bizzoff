0229d15ed6a2d1f92f1661374e50bc4c79b0ee77
= <faaizjamali@gmail.com>
2013-03-07 01:22:46 +0500

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,5 @@ CnsCardsys::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
    match ':controller(/:action(/:id))(.:format)'
-    root :to => 'access#index'
+    root :to => 'access#login'
+end
