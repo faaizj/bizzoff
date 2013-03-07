87e3d62278a45bf44b610623f21d9f28eae3912d
= <faaizjamali@gmail.com>
2013-03-07 12:55:07 +0500

    Mobile controller and views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CnsCardsys::Application.routes.draw do
  
+   
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +54,7 @@ CnsCardsys::Application.routes.draw do
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
-
+  
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
    match ':controller(/:action(/:id))(.:format)'
